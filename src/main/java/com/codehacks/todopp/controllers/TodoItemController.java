e68d3a2974bfbe12aec03a731c2a77aab979f32e
Rume7 <electengrhume@gmail.com>
2022-08-27 15:32:09 +0100

    Tweeked ToDo Controller

--- src/main/java/com/codehacks/todopp/controllers/TodoItemController.java
+++ src/main/java/com/codehacks/todopp/controllers/TodoItemController.java
@@ -2,14 +2,13 @@ package com.codehacks.todopp.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TodoItemController {
-    private final Logger logger = LoggerFactory.getLogger("TODOItem controller");
+    private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
 
     @GetMapping("/")
     public ModelAndView index() {
