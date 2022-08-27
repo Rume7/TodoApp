a4d9c2d440eb89435f81893d6a33a24f0fcd7b3d
Rume7 <electengrhume@gmail.com>
2022-08-27 18:36:27 +0100

    Added database content to modelAndView object

--- src/main/java/com/codehacks/todopp/controllers/TodoItemController.java
+++ src/main/java/com/codehacks/todopp/controllers/TodoItemController.java
@@ -1,7 +1,9 @@
 package com.codehacks.todopp.controllers;
 
+import com.codehacks.todopp.repositories.TodoItemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,10 +12,14 @@ import org.springframework.web.servlet.ModelAndView;
 public class TodoItemController {
     private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
 
-    @GetMapping("/")
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+
+    @GetMapping(value = "/index")
     public ModelAndView index() {
-        logger.debug("Request to GET index");
+        logger.info("Request to GET index");
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemRepository.findAll());
         return modelAndView;
     }
 }
